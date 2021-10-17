5f616e5563503ffdebb0a7729e06a8f01ee3e269
Fabiano Leite <fabiano.marechal@gmail.com>
2021-10-17 19:02:02 -0300

    migration to next,js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import './login.css'
-import Logo from '../../assets/logo_ache.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './login.module.css'
 import { MdEmail, MdLock } from 'react-icons/md'
 
 const Login = () => {
   return (
-    <div className="login">
-      <div className="login-left">
+    <div className={styles.login}>
+      <div className={styles.login_left}>
         <h1>Seja Bem-Vindo á</h1>
-        <img src={Logo} alt="logo teste" />
+        <Image src="/assets/logo_ache.png" alt="logo teste" width='110px' height='140px' />
         <h5>
           Ainda não tem acesso
-          <br /> ao sistema? <a href="#"> clique aqui.</a>
+          <br /> ao sistema? <Link href="#"><a>clique aqui.</a></Link>
         </h5>
       </div>
-      <div className="login_right">
+      <div className={styles.login_right}>
         <h1>Login para Dashboard</h1>
-        <div className="login-InputEmail">
+        <div className={styles.login_InputEmail}>
           <MdEmail />
           <input type="text" placeholder="Digite um email" />
         </div>
-        <div className="login-InputPassword">
+        <div className={styles.login_InputPassword}>
           <MdLock />
-          <input type="text" placeholder="Digite sua senha" />
+          <input type="password" placeholder="Digite sua senha" />
         </div>
-        <div className="box">
+        <div className={styles.box}>
           <label>
             <input type="checkbox" />
             Lembrar-me
           </label>
           <label >
-            <a href="#"> Esqueci a senha?</a>
+            <Link href="#"><a> Esqueci a senha?</a></Link>
           </label>
         </div>
         <button type="submit">Entrar</button>
