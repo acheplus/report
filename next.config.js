8e38adfd9687a95d83603a0389d07c28ef839a5a
Fabiano Leite <fabiano.marechal@gmail.com>
2021-11-29 22:24:30 -0300

    corrected merge (#15)
    * Corrected bug ocurred in the npm install
    
    * Imagem dentro do Input
    
    * Incremento tela login
    
    * versao login OK
    
    * versao login finalizada
    
    * versao login finalizada
    
    * versao login finalizada
    
    * Versao final Login OK (#8) (#9)
    
    * Corrected bug ocurred in the npm install
    
    * Imagem dentro do Input
    
    * Incremento tela login
    
    * versao login OK
    
    * versao login finalizada
    
    * versao login finalizada
    
    * versao login finalizada
    
    Co-authored-by: rvbelo <criatividadesign@gmail.com>
    
    Co-authored-by: rvbelo <criatividadesign@gmail.com>
    
    * Create README.md
    
    * export to CSV Ok
    
    * autenticacao necessario em paginas internas
    
    * adicionado INE as listagens
    
    * corrected destination
    
    * Update next.config.js
    
    Co-authored-by: rvbelo <criatividadesign@gmail.com>

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
     return [
       {
         source: '/api/v1/:path*',
-        // destination: 'http://acheplusreport-env.eba-rynbyimh.sa-east-1.elasticbeanstalk.com/api/v1/:path*'
-        destination: 'http://localhost:5000/api/v1/:path*'
+        destination: 'http://acheplusreport-env.eba-rynbyimh.sa-east-1.elasticbeanstalk.com/api/v1/:path*'
+        //destination: 'http://localhost:5000/api/v1/:path*'
       }
     ]
   }
